c7d6eacdbf96666c265ce2e3e4778801864869bd
Robert Krahn <robert.krahn@gmail.com>
2022-11-30 02:09:38 +0100

    add scoped profile capturer

--- src/global.rs
+++ src/global.rs
@@ -11,16 +11,24 @@ type Client = tracy_client::Client;
 #[allow(unused)]
 static INSTANCE: Lazy<Arc<Mutex<Option<Client>>>> = Lazy::new(Default::default);
 
-pub fn global_profiler_init() {
+#[allow(unused_variables)]
+pub fn global_profiler_init(thread_name: &str) {
     #[cfg(feature = "profile-with-optick")]
     {
         optick::start_capture();
         *INSTANCE.lock().unwrap() = Some(());
+        // Don't set thread_name as optick wants it's main thread to be called
+        // "MainThread" which it does itself.
     }
     #[cfg(feature = "profile-with-tracy")]
     {
         let client = tracy_client::Client::start();
         *INSTANCE.lock().unwrap() = Some(client);
+        set_thread_name(thread_name);
+    }
+    #[cfg(feature = "profile-with-superluminal")]
+    {
+        set_thread_name(thread_name);
     }
 }
 
@@ -47,5 +55,30 @@ pub fn set_thread_name(name: &str) {
     #[cfg(feature = "profile-with-tracy")]
     if let Some(client) = &*INSTANCE.lock().unwrap() {
         client.set_thread_name(name);
+    }
+}
+
+pub struct GlobalCapture {
+    capture_path: Option<String>,
+}
+
+impl GlobalCapture {
+    #[allow(unused_variables)]
+    pub fn init(
+        name: &str,
+        capture_path: Option<impl ToString>,
+    ) -> Self {
+        global_profiler_init(name);
+        Self {
+            capture_path: capture_path.map(|p| p.to_string()),
+        }
+    }
+}
+
+impl Drop for GlobalCapture {
+    fn drop(&mut self) {
+        if let Some(capture_path) = &self.capture_path {
+            global_profiler_stop(capture_path);
+        }
     }
 }
